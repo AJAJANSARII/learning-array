68ebb9cb1464c71e2830c8bdbf5b4ff9543f305b
AJAJANSARII <ajaj98071342@gmail.com>
2023-06-21 08:21:33 +0545

    created a student array and printed items on it

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("i am learning array")
+let student= ["ajaj" ,"faiyaj" ,"meraj","ajay","pooj","rani","sajid","sahim","soni","rezwan"]
+console.log(student[0])
+console.log(student[1])
+console.log(student[2])
+console.log(student[3])
+console.log(student[4])
+console.log(student[5])
+console.log(student[6])
+console.log(student[7])
+console.log(student[8])
+console.log(student[9])
+student[2]="ajaj"
+console.log(student[2])
+student[10]="sahim"
+console.log(student[10])
+console.log("****************************************")
+for (index in student){
+console.log(student [index])
+}
